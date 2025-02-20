d7b51275c3304a6659cf26bf1a93fc4de7b0f2cb
Raihanbujeee <bujexixi@gmail.com>
2025-02-20 09:07:59 +0700

    percobaan 3

---
+++ jobhseet2/MahasiswaMAin24.java
@@ -0,0 +1,21 @@
+package jobhseet2;
+
+public class MahasiswaMAin24 {
+    public static void main(String[] args) {
+        Mahasiswa24 mhsl = new Mahasiswa24();
+        mhsl.nama= "Muhammad Ali Farhan";
+        mhsl.nim = "2241720171";
+        mhsl.kelas = "SI 2J";
+        mhsl.ipk =  3.55;
+
+        mhsl.tampilkanInformasi();
+        mhsl.ubahKelas("SI 2K");
+        mhsl.updateIpk(3.60);
+        mhsl.tampilkanInformasi();
+
+        Mahasiswa24 mhs2 = new Mahasiswa24("Annisa Nabila","2141720160",3.25,"TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
+    }
+    
+}
