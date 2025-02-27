4bb5ea0cb5bdf6acf765d063474ca978dec36d32
Raihanbujeee <bujexixi@gmail.com>
2025-02-27 11:19:41 +0700

    percobaan 2 dan modifikasi

--- jobsheet3/Mahasiswa24.java
+++ jobsheet3/Mahasiswa24.java
@@ -3,4 +3,11 @@ import java.util.Scanner;
 public class Mahasiswa24 {
     String nim,nama,kelas;
     float ipk;
+    
+    void cetakInfo() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+    }
 }
